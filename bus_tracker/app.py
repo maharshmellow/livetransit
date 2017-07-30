cc898b427d500bf5bd85911878daaed9799038e1
Maharsh Patel <patel@maharsh.net>
2017-07-29 20:19:00 -0600

    rate limiting the api

--- bus_tracker/app.py
+++ bus_tracker/app.py
@@ -1,13 +1,19 @@
 from flask import Flask
 from flask import jsonify
 from live import getLiveData
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
 app = Flask(__name__)
+limiter = Limiter(app, key_func=get_remote_address)
 
 @app.route("/")
+@limiter.exempt
 def index():
     return("Hello Worlds")
 
 @app.route("/api/data")
+@limiter.limit("300/minute")
 def data():
     return jsonify(getLiveData())
 
