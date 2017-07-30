81194b16ccbd6430ee49d287e2965d3be7f13582
Maharsh Patel <patel@maharsh.net>
2017-07-29 21:23:53 -0600

    simple map centered on edmonton

--- bus_tracker/app.py
+++ bus_tracker/app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, url_for
 from flask import jsonify
 from live import getLiveData
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from flask import render_template
 
 app = Flask(__name__)
 limiter = Limiter(app, key_func=get_remote_address)
@@ -12,11 +13,15 @@ limiter = Limiter(app, key_func=get_remote_address)
 def index():
     return("Hello Worlds")
 
-@app.route("/api/data")
+@app.route("/api/data", methods=["GET"])
 @limiter.limit("300/minute")
 def data():
     return jsonify(getLiveData())
 
+@app.route("/test")
+def test():
+    return render_template("home.html")
+    # return(url_for("static", filename="css/styles.css"))
 
 if __name__ == "__main__":
     app.run(debug=True)
